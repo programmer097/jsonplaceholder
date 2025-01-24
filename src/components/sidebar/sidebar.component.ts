802c158d08c0804166843b5971335481fcec44e5
Aditya Sahu <sahuaditya083@gmail.com>
2025-01-24 09:11:23 +0000

    added icons for sidenav items

--- src/components/sidebar/sidebar.component.ts
+++ src/components/sidebar/sidebar.component.ts
@@ -1,21 +1,28 @@
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule, MatListModule, CommonModule, TitleCasePipe],
+  imports: [
+    RouterModule,
+    MatListModule,
+    CommonModule,
+    TitleCasePipe,
+    MatIconModule,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css',
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  readonly resources = [
-    { name: 'posts', path: '/posts' },
-    { name: 'comments', path: '/comments' },
-    { name: 'albums', path: '/albums' },
-    { name: 'photos', path: '/photos' },
-    { name: 'todos', path: '/todos' },
-    { name: 'users', path: '/users' },
+  resources = [
+    { name: 'posts', path: '/posts', icon: 'article' },
+    { name: 'comments', path: '/comments', icon: 'comment' },
+    { name: 'albums', path: '/albums', icon: 'album' },
+    { name: 'photos', path: '/photos', icon: 'photo' },
+    { name: 'todos', path: '/todos', icon: 'check_circle' },
+    { name: 'users', path: '/users', icon: 'person' },
   ];
 }
