0d0660931d6c63196816b3f49e9bcc195c84f742
Aditya Sahu <sahuaditya083@gmail.com>
2025-01-24 06:15:48 +0000

    added api endpoints and models.

--- src/models.ts
+++ src/models.ts
@@ -4,3 +4,53 @@ export interface Post {
   title: string;
   body: string;
 }
+
+export interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export interface Album {
+  userId: number;
+  id: number;
+  title: string;
+}
+
+export interface Photo {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+export interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -13,4 +13,24 @@ export class DataService {
     const path = `${this.basePath}/posts`;
     return this.http.get<Post[]>(path);
   }
+  getComments() {
+    const path = `${this.basePath}/comments`;
+    return this.http.get<Post[]>(path);
+  }
+  getAlbums() {
+    const path = `${this.basePath}/albums`;
+    return this.http.get<Post[]>(path);
+  }
+  getPhotos() {
+    const path = `${this.basePath}/photos`;
+    return this.http.get<Post[]>(path);
+  }
+  getTodos() {
+    const path = `${this.basePath}/todos`;
+    return this.http.get<Post[]>(path);
+  }
+  getUsers() {
+    const path = `${this.basePath}/users`;
+    return this.http.get<Post[]>(path);
+  }
 }
